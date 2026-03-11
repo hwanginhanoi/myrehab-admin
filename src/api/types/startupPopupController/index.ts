dc3ec388502b05aa7563a5bbbd6ad5ad66b4b073
Luu Tuan Hoang <hoanglt0104@gmail.com>
2026-03-11 19:41:03 +0700

    Update API

---
+++ src/api/types/startupPopupController/index.ts
@@ -0,0 +1,36 @@
+export type {
+  CreatePopup200,
+  CreatePopupMutationRequest,
+  CreatePopupMutationResponse,
+  CreatePopupMutation,
+} from './CreatePopup.ts'
+export type {
+  DeletePopupPathParams,
+  DeletePopup200,
+  DeletePopupMutationResponse,
+  DeletePopupMutation,
+} from './DeletePopup.ts'
+export type {
+  GetActivePopup200,
+  GetActivePopupQueryResponse,
+  GetActivePopupQuery,
+} from './GetActivePopup.ts'
+export type {
+  GetAllPopupsQueryParams,
+  GetAllPopups200,
+  GetAllPopupsQueryResponse,
+  GetAllPopupsQuery,
+} from './GetAllPopups.ts'
+export type {
+  GetPopupByIdPathParams,
+  GetPopupById200,
+  GetPopupByIdQueryResponse,
+  GetPopupByIdQuery,
+} from './GetPopupById.ts'
+export type {
+  UpdatePopupPathParams,
+  UpdatePopup200,
+  UpdatePopupMutationRequest,
+  UpdatePopupMutationResponse,
+  UpdatePopupMutation,
+} from './UpdatePopup.ts'
---
+++ src/api/zod/startupPopupController/index.ts
@@ -0,0 +1,30 @@
+export {
+  createPopup200Schema,
+  createPopupMutationRequestSchema,
+  createPopupMutationResponseSchema,
+} from './createPopupSchema.ts'
+export {
+  deletePopupPathParamsSchema,
+  deletePopup200Schema,
+  deletePopupMutationResponseSchema,
+} from './deletePopupSchema.ts'
+export {
+  getActivePopup200Schema,
+  getActivePopupQueryResponseSchema,
+} from './getActivePopupSchema.ts'
+export {
+  getAllPopupsQueryParamsSchema,
+  getAllPopups200Schema,
+  getAllPopupsQueryResponseSchema,
+} from './getAllPopupsSchema.ts'
+export {
+  getPopupByIdPathParamsSchema,
+  getPopupById200Schema,
+  getPopupByIdQueryResponseSchema,
+} from './getPopupByIdSchema.ts'
+export {
+  updatePopupPathParamsSchema,
+  updatePopup200Schema,
+  updatePopupMutationRequestSchema,
+  updatePopupMutationResponseSchema,
+} from './updatePopupSchema.ts'
