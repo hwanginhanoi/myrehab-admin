4f81334fdcdd50e6474a4841b55d338c911c6eef
Luu Tuan Hoang <hoanglt0104@gmail.com>
2026-03-18 14:40:26 +0700

    Multilang

--- src/api/types/doctorController/index.ts
+++ src/api/types/doctorController/index.ts
@@ -1,12 +1,73 @@
-export type { ApproveCourseRequestPathParams, ApproveCourseRequest200, ApproveCourseRequestMutationRequest, ApproveCourseRequestMutationResponse, ApproveCourseRequestMutation } from "./ApproveCourseRequest.ts";
-export type { AssignCourseToPatientPathParams, AssignCourseToPatient200, AssignCourseToPatientMutationRequest, AssignCourseToPatientMutationResponse, AssignCourseToPatientMutation } from "./AssignCourseToPatient.ts";
-export type { CreateAndAssignCustomCoursePathParams, CreateAndAssignCustomCourse200, CreateAndAssignCustomCourseMutationRequest, CreateAndAssignCustomCourseMutationResponse, CreateAndAssignCustomCourseMutation } from "./CreateAndAssignCustomCourse.ts";
-export type { GetCourseRequestsQueryParamsStatusEnumKey, GetCourseRequestsQueryParams, GetCourseRequests200, GetCourseRequestsQueryResponse, GetCourseRequestsQuery } from "./GetCourseRequests.ts";
-export type { GetMyCourseAssignmentsQueryParams, GetMyCourseAssignments200, GetMyCourseAssignmentsQueryResponse, GetMyCourseAssignmentsQuery } from "./GetMyCourseAssignments.ts";
-export type { GetMyPatientsQueryParams, GetMyPatients200, GetMyPatientsQueryResponse, GetMyPatientsQuery } from "./GetMyPatients.ts";
-export type { GetMyTrainersQueryParams, GetMyTrainers200, GetMyTrainersQueryResponse, GetMyTrainersQuery } from "./GetMyTrainers.ts";
-export type { GetPatientCoursesPathParams, GetPatientCoursesQueryParams, GetPatientCourses200, GetPatientCoursesQueryResponse, GetPatientCoursesQuery } from "./GetPatientCourses.ts";
-export type { RejectCourseRequestPathParams, RejectCourseRequest200, RejectCourseRequestMutationRequest, RejectCourseRequestMutationResponse, RejectCourseRequestMutation } from "./RejectCourseRequest.ts";
-export type { RevokeCourseAssignmentPathParams, RevokeCourseAssignment200, RevokeCourseAssignmentMutationResponse, RevokeCourseAssignmentMutation } from "./RevokeCourseAssignment.ts";
-export type { SearchMyPatientsQueryParams, SearchMyPatients200, SearchMyPatientsQueryResponse, SearchMyPatientsQuery } from "./SearchMyPatients.ts";
-export { getCourseRequestsQueryParamsStatusEnum } from "./GetCourseRequests.ts";
+export type {
+  ApproveCourseRequestPathParams,
+  ApproveCourseRequest200,
+  ApproveCourseRequestMutationRequest,
+  ApproveCourseRequestMutationResponse,
+  ApproveCourseRequestMutation,
+} from './ApproveCourseRequest.ts'
+export type {
+  AssignCourseToPatientPathParams,
+  AssignCourseToPatient200,
+  AssignCourseToPatientMutationRequest,
+  AssignCourseToPatientMutationResponse,
+  AssignCourseToPatientMutation,
+} from './AssignCourseToPatient.ts'
+export type {
+  CreateAndAssignCustomCoursePathParams,
+  CreateAndAssignCustomCourse200,
+  CreateAndAssignCustomCourseMutationRequest,
+  CreateAndAssignCustomCourseMutationResponse,
+  CreateAndAssignCustomCourseMutation,
+} from './CreateAndAssignCustomCourse.ts'
+export type {
+  GetCourseRequestsQueryParamsStatusEnumKey,
+  GetCourseRequestsQueryParams,
+  GetCourseRequests200,
+  GetCourseRequestsQueryResponse,
+  GetCourseRequestsQuery,
+} from './GetCourseRequests.ts'
+export type {
+  GetMyCourseAssignmentsQueryParams,
+  GetMyCourseAssignments200,
+  GetMyCourseAssignmentsQueryResponse,
+  GetMyCourseAssignmentsQuery,
+} from './GetMyCourseAssignments.ts'
+export type {
+  GetMyPatientsQueryParams,
+  GetMyPatients200,
+  GetMyPatientsQueryResponse,
+  GetMyPatientsQuery,
+} from './GetMyPatients.ts'
+export type {
+  GetMyTrainersQueryParams,
+  GetMyTrainers200,
+  GetMyTrainersQueryResponse,
+  GetMyTrainersQuery,
+} from './GetMyTrainers.ts'
+export type {
+  GetPatientCoursesPathParams,
+  GetPatientCoursesQueryParams,
+  GetPatientCourses200,
+  GetPatientCoursesQueryResponse,
+  GetPatientCoursesQuery,
+} from './GetPatientCourses.ts'
+export type {
+  RejectCourseRequestPathParams,
+  RejectCourseRequest200,
+  RejectCourseRequestMutationRequest,
+  RejectCourseRequestMutationResponse,
+  RejectCourseRequestMutation,
+} from './RejectCourseRequest.ts'
+export type {
+  RevokeCourseAssignmentPathParams,
+  RevokeCourseAssignment200,
+  RevokeCourseAssignmentMutationResponse,
+  RevokeCourseAssignmentMutation,
+} from './RevokeCourseAssignment.ts'
+export type {
+  SearchMyPatientsQueryParams,
+  SearchMyPatients200,
+  SearchMyPatientsQueryResponse,
+  SearchMyPatientsQuery,
+} from './SearchMyPatients.ts'
+export { getCourseRequestsQueryParamsStatusEnum } from './GetCourseRequests.ts'
